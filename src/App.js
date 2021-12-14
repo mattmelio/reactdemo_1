5504b33a2fa71bd89ef221b4f0b199a7dfdf1a5a
mattmelio <70667820+mattmelio@users.noreply.github.com>
2021-12-13 23:49:01 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Todo from './components/Todo';
+
+
+function App() {
+  return (
+    <div>
+      <h1>My ToDo's</h1>
+      <Todo text='Learn React'/>  
+      <Todo text='Master React'/>
+      <Todo text='Explore the full react course  React'/>
+      
+    </div>
+  );
+}
+
+export default App;
